2bd262302ade33d9dd344955e9e718a3c84297df
jdeu <jdeu@pc-jdeu>
2015-01-14 14:19:39 +0100

    Entrades arbre prova

--- web/js/entradesNoticies.js
+++ web/js/entradesNoticies.js
@@ -1,10 +1,2 @@
 
-$(document).ready(function () {
-//Acció en pitjar el botó nova entrada
-    $('#boto_novaentrada').on('click', function (event) {
-        //$("#cos_pagina").load("nova_noticia.html");
-        $("#cos_pagina").hide();
-        $("#cos_pagina2").load("nova_noticia.html");
-    });
 
-});
