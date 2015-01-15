1d4e1fb1d8d96c727c0c6452a535058393547a11
jdeu <jdeu@pc-jdeu>
2015-01-15 14:01:04 +0100

    alguns canvis

--- web/js/noticies.js
+++ web/js/noticies.js
@@ -1,5 +1,11 @@
+/*
+	He agrupat tot el que fa referència a Notícies perquè, vist que sempre és el mateix HTML, tenir centralitzat tot el javascript per
+	no repetir variables i funcions.
+*/
 
 $(document).ready(function () {
+
+	//Acció en pitjar el botó accepta a nova noticia
     $("#formulariNovaNoticia").submit(function () {
        // alert("Formulari d'enviament de dades al servidor mysql");
         var $form = $(this),
@@ -9,11 +15,13 @@ $(document).ready(function () {
         //alert("I es troba al carrer " + campCos);
         doAltaFa(campTitol, campCos);
         //Recarregam la pàgina anterior dinàmicament
-        $("#cos_pagina").load("nova_noticia.html"); 
+        
+		window.location.replace("farmacia.html"); 
         return false;
     }
     );
 
+	//Acció en pitjar el botó accepta a edita noticia
     $("#formulariEditaNoticia").submit(function () {
        // alert("Formulari d'enviament de dades al servidor mysql");
         var $form = $(this),
@@ -23,13 +31,35 @@ $(document).ready(function () {
         //alert("I es troba al carrer " + campCos);
         doAltaFa(campTitol, campCos);
         //Recarregam la pàgina anterior dinàmicament
-        $("#cos_pagina").load("nova_noticia.html"); 
+		alert("edita")
+		window.location.replace("farmacia.html"); 
         return false;
     }
     );
 
 
+	//Acció en pitjar el botó nova noticia
+	$('#boto_novanoticia').on('click', function(event) {
+		$("#cos_pagina").load("nova_noticia.html"); 
+	});
+
+	//Acció en pitjar el botó de cancel·la dins nova noticia
     $('#boto_novanoticia_cancela').on('click', function (event) {
-        $("#cos_pagina").load("nova_noticia.html"); 
+		window.location.replace("farmacia.html"); 
     });
 });
+
+
+//Acció en pitjar el botó edita notícia. Li passam per paràmetre la ID de la notícia.
+function edita_noticia(id_noticia)
+{	
+	id_edita=id_noticia;
+	$("#cos_pagina").load("edita_noticia.html"); 
+}
+
+function elimina_noticia(id_noticia)
+{	
+	alert (id_noticia);
+}
+
+
