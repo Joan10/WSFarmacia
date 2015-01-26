b7496c5580659910bb5e44e89a1cad5c845b9b58
javi <javi@192.168.1.129>
2015-01-26 17:39:25 +0100

    Llistar les notícies, Encara no està acabat..

--- web/js/farmacia.js
+++ web/js/farmacia.js
@@ -3,12 +3,14 @@
 $(document).ready(function () {
     llistatNoticies = doListaNo();
     alert(llistatNoticies);
-    arrayParametres = [];
+    /*arrayParametres = [];
+    //alert('prova');
 
     llistatNoticies.search.replace('LTIMNL', '').split('@@').forEach(function (val) {
         split = val.split("=");
+        alert(split);
         $.each(split, function (indice, valor) {
             arrayParametres.push(valor);
         });
-    });
+    });*/
 });
