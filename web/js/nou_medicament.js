20a048f32ba4d3783fafafc61ff23ef7d9f4eeb4
javi <javi@Lenovo02>
2015-02-11 19:01:00 +0100

    Lo que vulguis

--- web/js/nou_medicament.js
+++ web/js/nou_medicament.js
@@ -1,15 +1,12 @@
 $(document).ready(function () {
-    alert("S'ha carregat 'nou_medicament.html'");
     $("#formulari_nou_medicament").submit(function () {
-    //    alert('formulari del nou medicament');
         var $form = $(this),
                 campNom = $form.find("input[name='nom']").val(),
                 campDescripcio = $form.find("textarea[name='descripcio']").val(),
-                campCategoria = $form.find("select[name='categoria']").val();
-      //  alert(campNom);
-    //    alert(campDescripcio);
-      //  alert(campCategoria);
-        doAltaMe();
+                campCategoria = $form.find("select[name='categoria']").val(),
+                campCodiMedicament = $form.find("input[name='codiMedicament']").val(),
+                campQuantitatMedicament = $form.find("input[name='quantitatMedicament']").val();
+        doAltaMe(campCodiMedicament,campNom,campCategoria,campDescripcio,campQuantitatMedicament);
         return false;
     });
 });
